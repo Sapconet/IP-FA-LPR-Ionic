b628ad58113a73ff1e5afba41e21adf9d55834bc
Zavast7 <50820754+Zavast7@users.noreply.github.com>
2019-11-25 15:36:33 +0200

    added settings page, camera input switch and menu

--- ionic-licplat-demo/image-display/src/app/app.component.ts
+++ ionic-licplat-demo/image-display/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { MenuController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
@@ -23,5 +26,10 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  openMenu() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
   }
 }
--- ionic-licplat-demo/image-display/src/app/tab2/tab2.page.ts
+++ ionic-licplat-demo/image-display/src/app/tab2/tab2.page.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { PhotoService } from "../services/photo.service";
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
+import { PhotoService } from '../services/photo.service';
 import { VideoService } from '../services/video.service';
 
 // import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture';
 
 @Component({
-  selector: "app-tab2",
-  templateUrl: "tab2.page.html",
-  styleUrls: ["tab2.page.scss"]
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
 })
 
 export class Tab2Page implements OnInit {
   currentImage: any;
   // private mediaCapture: MediaCapture;
+  booleanValue: boolean = true;
 
-  constructor(public photoService: PhotoService, public videoService: VideoService) { }
+  constructor(public photoService: PhotoService, public videoService: VideoService, public toastController: ToastController) { }
 
   ngOnInit() {
     this.photoService.loadSaved();
-    console.log("Loading images...");
+    console.log('Loading images...');
   }
 
   doRefresh(event) {
@@ -29,6 +31,26 @@ export class Tab2Page implements OnInit {
     setTimeout(() => {
       console.log('Async operation has ended');
       event.target.complete();
-    }, 2000); 
+    }, 2000);
+  }
+
+  switchInput(event) {
+    this.booleanValue = !this.booleanValue;
+
+    if (this.booleanValue === true) {
+      this.presentToast('Input switched to Still Images');
+    } else {
+      this.presentToast('Input switched to Video Capturing');
+    }
+
+  }
+
+  async presentToast(text) {
+    const toast = await this.toastController.create({
+      message: text,
+      position: 'bottom',
+      duration: 2000
+    });
+    toast.present();
   }
 }
